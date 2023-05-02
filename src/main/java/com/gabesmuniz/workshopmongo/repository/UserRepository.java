0c6ec71eee9d20d25c378d429197b1319ee43d66
Gabriel Muniz Lopes <oficial.gabrielmlopes@gmail.com>
2023-05-02 20:58:31 -0300

    Connecting to MongoDB with Repository and Service

---
+++ src/main/java/com/gabesmuniz/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.gabesmuniz.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gabesmuniz.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
