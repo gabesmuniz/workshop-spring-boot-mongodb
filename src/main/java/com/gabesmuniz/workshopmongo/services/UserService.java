ae57bcbfbd7ca50a71c76813dbd636aa54d44122
Gabriel Muniz Lopes <oficial.gabrielmlopes@gmail.com>
2023-05-03 14:27:34 -0300

    Getting User by ID and added Error 404

--- src/main/java/com/gabesmuniz/workshopmongo/services/UserService.java
+++ src/main/java/com/gabesmuniz/workshopmongo/services/UserService.java
@@ -1,21 +1,28 @@
 package com.gabesmuniz.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabesmuniz.workshopmongo.domain.User;
 import com.gabesmuniz.workshopmongo.repository.UserRepository;
+import com.gabesmuniz.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
 
+	// erro com findOne
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
 }
