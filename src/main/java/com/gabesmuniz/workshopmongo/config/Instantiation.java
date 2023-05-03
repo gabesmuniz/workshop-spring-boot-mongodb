d42a2f223b785b0707e1944ed76de700a834ef87
Gabriel Muniz Lopes <oficial.gabrielmlopes@gmail.com>
2023-05-03 18:20:47 -0300

    Query with Multiple Criteria

--- src/main/java/com/gabesmuniz/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gabesmuniz/workshopmongo/config/Instantiation.java
@@ -39,8 +39,8 @@ public class Instantiation implements CommandLineRunner{
 	
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/20218"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/20218"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		CommentDTO c1 = new CommentDTO("Boa Viagem!!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
 		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("21/03/2018"), new AuthorDTO(bob));
